9e0b4480dbe833ce9c975881a115e493a180b726
dichanshrestha <blackuniverse.808@gmail.com>
2025-05-12 11:27:56 +0545

    fix: Service layout

--- src/app/(Home)/_components/Services.tsx
+++ src/app/(Home)/_components/Services.tsx
@@ -82,9 +82,9 @@ export function Services() {
   }
 
   return (
-    <div className="w-screen relative left-1/2 -translate-x-1/2 mb-10 flex items-center justify-center bg-[#F5F7FE] overflow-hidden">
+    <div className="w-screen relative left-1/2 -translate-x-1/2 mb-10 bg-[#F5F7FE] overflow-hidden">
       <div>
-        <div className="flex flex-col items-start justify-center pl-9 pt-10">
+        <div className="flex flex-col items-start justify-center lg:pl-9 pt-10">
           <motion.h1
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -107,7 +107,7 @@ export function Services() {
           </motion.h1>
         </div>
 
-        <div className="flex w-full items-start mt-10 justify-start">
+        <div className="flex w-full items-start mt-10 justify-start lg:pl-9">
           <TabCarousel
             setActiveCategory={setActiveCategory}
             activeIndex={activeIndex}
@@ -115,7 +115,7 @@ export function Services() {
           />
         </div>
 
-        <div className="mt-4 mb-10 flex flex-wrap gap-8 ml-4 md:ml-0 w-full items-center lg:pl-4">
+        <div className="mt-4 mb-10 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1gap-8 ml-4 md:ml-0 w-full items-center lg:pl-9">
           {data.map((item, index) => (
             <div
               key={index}
@@ -133,7 +133,7 @@ export function Services() {
         </div>
 
         {showList && !showAll && (
-          <div className="flex justify-end w-full mb-10">
+          <div className="flex w-full mb-10 lg:pr-[52px] justify-end">
             <AnimatedButton
               onClick={() => {
                 setShowAll(true);
